e5f4151f31cdd304d8863f9ddadb7076921185ca
jen-zecena <jzecenay@gmail.com>
2021-04-24 19:31:42 -0700

    Input.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,6 +2,7 @@ import React from "react";
 
 function Input(props){
     return <input type={props.type} placeholder = {props.placeholder}/>;
+    //TODO: delete this function if not needed
 }
 
 export default Input;
