d628cc930cae63134dc180570b169acfa370fae7
hfaara18 <faarahussein@gmail.com>
2021-04-25 21:17:14 -0700

    Done writing code for subscription actions. Follow up with code clean up and testing.

--- src/actions/subscriptions.js
+++ src/actions/subscriptions.js
@@ -19,7 +19,7 @@ import {
   The method dispatches the result of the action to the reducer, which
   informs the web app of the added subscription.
 */
-export default addSubscription = (followerID, followedID) => {
+export default addSubscription = ({ followerID, followedID }) => {
   /*
     1. Create the request configuration and stringify the parameters
     2. Make a request to the backend to add the new subscription.
@@ -27,16 +27,11 @@ export default addSubscription = (followerID, followedID) => {
     success and pass along user information
     4. otherwise, tell the authentication reducer about the failed attempt.
   */
-  try {
-    dispatch({
-      type: ,
-      payload: response.data
-    });
-  } catch (error) {
-    dispatch({
-      type:
-    })
-  }
+  const response = await axios.post(`/subscriptions`, { followerID, followedID }, tokenConfig(getState));
+  dispatch({
+    type: ADD_SUBSCRIPTION,
+    payload: response.data
+  });
 }
 
 /**
@@ -56,16 +51,11 @@ export default getSubscriptions = (followerID) => {
     success and pass along user information
     4. otherwise, tell the authentication reducer about the failed attempt.
   */
-  try {
-    dispatch({
-      type: ,
-      payload: response.data
-    });
-  } catch (error) {
-    dispatch({
-      type:
-    })
-  }
+  const response = await axios.post(`/users/${followerID}/subscriptions`, tokenConfig(getState));
+  dispatch({
+    type: GET_SUBSCRIPTIONS,
+    payload: response.data
+  });
 }
 
 /**
@@ -85,21 +75,17 @@ export default getSubscribers = (followedID) => {
     success and pass along user information
     4. otherwise, tell the authentication reducer about the failed attempt.
   */
-  try {
-    dispatch({
-      type: ,
-      payload: response.data
-    });
-  } catch (error) {
-    dispatch({
-      type:
-    })
-  }
+  const response = await axios.get(`/users/${followedID}/subscribers/`, tokenConfig(getState));
+  dispatch({
+    type: GET_SUBSCRIBERS,
+    payload: response.data
+  });
 }
 
 
 /**
-  @param subscriptionID: the id of the subscription object
+  @param followerID: the id of the user who is doing the following
+  @param followedID: the id of the user being followed
 
   This method deletes a subscription by making a request to the backend to delete the
   specified subscription from the database.
@@ -107,7 +93,7 @@ export default getSubscribers = (followedID) => {
   The method dispatches the result of the action to the reducer, which
   informs the web app of the deleted subscription.
 */
-export default deleteSubscription = (subscriptionID) => {
+export default deleteSubscription = ({ followerID, followedID }) => {
   /*
     1. Create the request configuration and stringify the parameters
     2. Make a request to the backend to delete the specified subscription.
@@ -115,14 +101,9 @@ export default deleteSubscription = (subscriptionID) => {
     success and pass along user information
     4. otherwise, tell the authentication reducer about the failed attempt.
   */
-  try {
-    dispatch({
-      type: ,
-      payload: response.data
-    });
-  } catch (error) {
-    dispatch({
-      type:
-    })
-  }
+  const response = await axios.delete(`/subscriptions`, { followerID, followedID }, tokenConfig(getState));
+  dispatch({
+    type: DELETE_SUBSCRIPTION,
+    payload: response.data
+  });
 }
