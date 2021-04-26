abab28798f24283d67b78ff3c3b60b652c4c0c71
hfaara18 <faarahussein@gmail.com>
2021-04-25 20:26:31 -0700

    Adding the possible action types

---
+++ src/actions/types/rsvps.js
@@ -0,0 +1,5 @@
+export const ADD_RSVP = 'ADD_RSVP';
+export const DELETE_RSVP = 'DELETE_RSVP';
+export const GET_USER_RSVPS = 'GET_USER_RSVPS';
+export const GET_EVENT_RSVPS = 'GET_EVENT_RSVPS';
+export const GET_ATTENDEES = 'GET_ATTENDEES';
