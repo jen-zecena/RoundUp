0316ff724a156598089dd96e0c275fac1183fd16
peibarthwu@gmail.com <mbarthwu@gmail.com>
2021-05-02 13:19:13 -0700

    user search

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -7,17 +7,17 @@ function EventCard(props){
 
   return (
     <Card style = {{ width: '18rem'}} >
-    <Card.Img variant = "top" src = "https://picsum.photos/200" / >
-    < Card.Body >
-      <Card.Title > {props.name} < /Card.Title>
+    <Card.Img variant = "top" src = "https://picsum.photos/200" />
+    <Card.Body>
+      <Card.Title > {props.name} </Card.Title>
       <Card.Subtitle className="mb-2 text-muted">
         {props.date}{' '}{props.time}{' '}{props.location}
       </Card.Subtitle>
       <Card.Text >
         {props.description}
-
       </Card.Text>
-      <Button variant="danger" > More Information < /Button>
+      <Button variant="danger" > More Information 
+      </Button>
     </Card.Body>
   </Card>
   );
