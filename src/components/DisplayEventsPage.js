bae03884325c8e240f00a159f8cf8aad7f6c5414
jen-zecena <jzecenay@gmail.com>
2021-05-02 14:27:11 -0700

    Event Sorting and Filtering Done

--- src/components/DisplayEventsPage.js
+++ src/components/DisplayEventsPage.js
@@ -1,42 +1,46 @@
-import React, { useState } from "react";
+import React from "react";
 import events from "../events";
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import {Link} from "react-router-dom";
 
-console.log(events);
-//
-// function sortEventsByDate(prop){
-//   return function(a, b) {
-//         if (a[prop] > b[prop]) {
-//             return 1;
-//         } else if (a[prop] < b[prop]) {
-//             return -1;
-//         }
-//         return 0;
-//     }
-// }
-//
-// const sortedEvents = array.sort(sortEventsByDate("date"));;
+function sortEvents(eventsObj){
+  console.log("original list of events:")
+  console.log(eventsObj);
+
+  return eventsObj.slice().sort((a,b) => (a.eventTime > b.eventTime) ? 1: -1);
+
+}
+
+var sortedEvents = sortEvents(events);
+console.log("sorted list of events");
+console.log(sortedEvents);
+
+function filterEvents(eventsObj){
+  var currentDate = new Date();
+  var dateTime = currentDate.getFullYear() + "-" + currentDate.getMonth() + "-" + currentDate.getDate() +
+                 " " + currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds()
+
+  console.log(dateTime);
+  console.log(currentDate);
+  console.log(currentDate.getFullYear());
+
+
+  return sortedEvents.filter(function(e){
+    return e.eventTime > dateTime;
+  });
+}
+
+
+
+var filteredEvents = filterEvents(sortedEvents);
+
+console.log("filtered Events");
+console.log(filteredEvents);
+
 
 
 const DisplayEventsPage = () => {
-
-  const [data, setData] = useState([]);
-
-  const sortArray = type => {
-      const types = {
-        date: 'date',
-      };
-      const sortProperty = types[type];
-      const sorted = events.sort((a, b) => b[sortProperty] - a[sortProperty]);
-      console.log(sorted);
-      setData(sorted);
-    };
-
-  const sortedEvents = sortArray('date');
-  console.log(sortedEvents);
-
 
 
   return (<div>
@@ -44,13 +48,13 @@ const DisplayEventsPage = () => {
       Events Page
     </h1>
       <dl className="dictionary">
-      {events.map((event, index) => (
-        <Card style = {{ width: '18rem'}} >
+      {filteredEvents.map((event, index) => (
+        <Card style = {{ width: '18rem'}} key ={index} >
         <Card.Img variant = "top" src = "https://picsum.photos/200" / >
         < Card.Body >
           <Card.Title > {event.name} < /Card.Title>
           <Card.Subtitle className="mb-2 text-muted">
-            {event.date}{' '}{event.time}{' '}{event.location}
+            {event.eventTime}{' '}{event.location}
           </Card.Subtitle>
           <Card.Text >
             {event.description}
