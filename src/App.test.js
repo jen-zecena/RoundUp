0de8fbf3cb8033604ae16846736a1d5f273d7c8e
peibarthwu <54913759+peibarthwu@users.noreply.github.com>
2021-05-06 02:56:08 -0700

    testing files for components

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,23 @@
-import {getRequest, handleChange, RSVPForm, EventPosterCardWithRSVP, userRSVP} from "./DisplayEventPoster"
+import UploadEvent from './components/UploadEvent';
+import React from "react";
+import { shallow, configure, mount } from "enzyme";
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-test('handleChange', () => {
-    
-    expect(true).toBeTruthy();
-})
+configure({ adapter: new Adapter() });
 
+describe("UploadEvent", () => {
+    it("renders correctly", () => {
+      const wrapper = shallow(<UploadEvent />);
+      expect(wrapper.exists()).toBe(true);
+    });
+
+    it("updates name correctly", () => {
+        const wrapper = mount(<UploadEvent/>);
+        const input = wrapper.find('input[name="name"]');
+        input.instance().value = 'pei pei';
+        input.simulate('change');
+        expect(wrapper.state().name).toEqual('pei pei');      
+    });
+   
+
+});
