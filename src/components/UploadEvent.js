076b5cb2a0f6739ae8d499816be7aabdad2ae3e4
peibarthwu <54913759+peibarthwu@users.noreply.github.com>
2021-05-06 10:50:08 -0700

    upload event form fixed to work with redux

--- src/components/UploadEvent.js
+++ src/components/UploadEvent.js
@@ -7,20 +7,11 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Multiselect } from 'multiselect-react-dropdown';
 
+import { addEventAction } from '../actions/eventActions';
+import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
 
-function UploadEvent(){
-  const [eventInfo, setEventInfo] = useState({
-    uID: "",
-    description: "",
-    eventTime: "",
-    poster: "",
-    name: "",
-    location:"",
-    email:"", 
-    tags:""
-  });
-
-  const tags = [{name: 'Africana Studies'},
+const tags = [{name: 'Africana Studies'},
   {name: 'American Studies'},
   {name: 'Anthropology'},
   {name: 'Art'},
@@ -43,299 +34,127 @@ function UploadEvent(){
   {name: 'Geology'},
   ]
 
-  const requiredProps = ["uID" , "eventTime" , "name" , "location", "email"];
+class UploadEvent extends React.Component {
+
   
-  console.log(eventInfo);
 
-  function handleChange(event) {
-    const { name, value } = event.target;
-
-    setEventInfo(prevValue => {
-      if (name === "uID") {
-        return {
-          uID: value,
-          description: prevValue.description,
-          eventTime: prevValue.eventTime,
-          poster: prevValue.poster,
-          name: prevValue.name,
-          location: prevValue.location,
-          email: prevValue.email,
-          tags: prevValue.tags
-        };
-      } else if (name === "description") {
-        return {
-          uID: prevValue.uID,
-          description: value,
-          eventTime: prevValue.eventTime,
-          poster: prevValue.poster,
-          name: prevValue.name,
-          location: prevValue.location,
-          email: prevValue.email
-        };
-      } else if (name === "eventDate") {
-        const dateFormat = value;
-        return {
-          uID: prevValue.uID,
-          description: prevValue.description,
-          eventTime: dateFormat ,
-          poster: prevValue.poster,
-          name: prevValue.name,
-          location: prevValue.location,
-          email: prevValue.email
-        };
-      } else if (name === "eventTime") {
-        return {
-          uID: prevValue.uID,
-          description: prevValue.description,
-          eventTime: value,
-          poster: prevValue.poster,
-          name: prevValue.name,
-          location: prevValue.location,
-          email: prevValue.email,
-          tags: prevValue.tags
-        };
-      } else if (name === "poster") {
-        return {
-          uID: prevValue.uID,
-          description: prevValue.description,
-          eventTime: prevValue.eventTime,
-          poster: value,
-          name: prevValue.name,
-          location: prevValue.location,
-          email: prevValue.email,
-          tags: prevValue.tags
-        };
-      }else if (name === "name") {
-        return {
-          uID: prevValue.uID,
-          description: prevValue.description,
-          eventTime: prevValue.eventTime,
-          poster: prevValue.poster,
-          name: value,
-          location: prevValue.location,
-          email: prevValue.email,
-          tags: prevValue.tags
-        };
-      }else if (name === "location") {
-        return {
-          uID: prevValue.uID,
-          description: prevValue.description,
-          eventTime: prevValue.eventTime,
-          poster: prevValue.poster,
-          name: prevValue.name,
-          location: value,
-          email: prevValue.email,
-          tags: prevValue.tags
-        };
-      }else if (name === "email") {
-        return {
-          uID: prevValue.uID,
-          description: prevValue.description,
-          eventTime: prevValue.eventTime,
-          poster: prevValue.poster,
-          name: prevValue.name,
-          location: prevValue.location,
-          email: value,
-          tags: prevValue.tags
-        };
-      }else if (name === "poster") {
-        return {
-          userID: prevValue.userID,
-          description: prevValue.description,
-          eventTime: prevValue.eventTime,
-          poster: event.target.files[0],
-          name: prevValue.name,
-          location: prevValue.location,
-          email: prevValue.email,
-          tags: prevValue.tags
-        };
-      }
-    });
-  }
-
-  /**
-   * 
-   * @param {*} selectedList 
-   *      The items selected by the user
-   * 
-   * Sets the tags field of the state
-   */
-  function onTagSelect(selectedList){
-    setEventInfo(prevValue => {
-        return {
-          userID: prevValue.userID,
-          description: prevValue.description,
-          eventTime: prevValue.eventTime,
-          poster: prevValue.poster,
-          name: prevValue.name,
-          location: prevValue.location,
-          email: prevValue.email,
-          tags: selectedList
-        };
-        });
-  }
-
-  /**
-   * 
-   * @param {*} none
-   * @returns 
-   *     TRUE if all required properties are non null
-   *     FALSE otherwise
-   *     REPORTS ERRORS TO USER
-   */
-  function checkRequiredFields(){
-      let errors = "";
-      let formIsValid = true;
-
-      //UserID
-      if(!eventInfo.userID){
-    //  formIsValid = false;
-    //  errors = errors + "userID, ";
-      }
-
-      //Date and Time
-      if(!eventInfo.eventTime){
-        formIsValid = false;
-        errors = errors + "event date, ";
-      }
-
-      //Name
-      if(!eventInfo.name){
-        formIsValid = false;
-        errors = errors + "name, ";
-      }
-      else if(eventInfo.name){
-          if(eventInfo.name.match(/^[a-zA-Z]+$/)){
-            formIsValid = false;
-            errors = errors + "name, ";
-          }        
-      }
-
-      //Email
-      if(!eventInfo.email){
-          formIsValid = false;
-          errors = errors + "email, ";
+  renderField = ({ input, label, type, placeholder, value, meta: { touched, error } }) => {
+    return (
+      <div className={`form-group ${touched && error ? 'error' : ''}`}>
+        <label>{label}</label>
+        {type === 'hidden'
+          ?
+        <input {...input} className='form-control' type={type} placeholder={placeholder} value={value} />
+          :
+        <input {...input} className='form-control' type={type} placeholder={placeholder} />
         }
+        {touched && error && (
+          <span className='text text-danger'>{error}</span>
+        )}
+      </div>
+    );
+  };
 
-      if(eventInfo.email !== "undefined"){
-          let lastAtPos = eventInfo.email.lastIndexOf('@');
-          let lastDotPos = eventInfo.email.lastIndexOf('.');
-
-          if (!(lastAtPos < lastDotPos && lastAtPos > 0 && eventInfo.email.indexOf('@@') === -1 && lastDotPos > 2 && (eventInfo.email.length - lastDotPos) > 2)) {
-            formIsValid = false;
-          }
-      } 
-
-
-      if (errors !== "") {
-        errors += "are empty or have errors";
-        alert(errors);
-      }
-      return formIsValid;
+  onSubmit = formValues => {
+    const eID = this.props.eID;
+    const time = new Date().getTime();
+    this.props.addEventAction({...formValues});
   }
 
-  /**
-		@param event: submit button is hit
-		Make sure all information is there and send PUT request
-		**/
-  function handleSubmit(event){
-    event.preventDefault();
-		if( checkRequiredFields()){
-      alert('An event was submitted.');
-      console.log(eventInfo);
-    }
-	} 
-
-  
+  render() {
     return (
       <div className="container">
-        <Container >
-
-        <Jumbotron>
-        <div>
-        <h1 style={{textAlign:"center"}}>
-          Upload an Event
-        </h1>
-        </div>
-        <br/>
-        <br/>
-          <Container className ="justify-content-center">
-          <Row>
-          <Col>
-          <Form>
-          <Form.Group controlId="eventName">
-            <Form.Label>Event Name</Form.Label>
-            <Form.Control
-              onChange = {handleChange}
-              name="name"
-              type="name"
-              placeholder="First Name" />
-          </Form.Group>
-
-          <Form.Group controlId="eventTime">
-            <Form.Label>Event Date</Form.Label>
-            <Form.Control
-              onChange = {handleChange}
-              name="eventTime"
-              type="datetime-local"
-              placeholder="Choose Event Date" />
-          </Form.Group>
-
-          <Form.Group controlId="eventDescription">
-            <Form.Label>Event Description</Form.Label>
-            <Form.Control
-              onChange = {handleChange}
-              as="textarea"
+        <Container>
+          <h1>
+            Add Event Form
+          </h1>
+          <form
+          onSubmit={this.props.handleSubmit(this.onSubmit)}
+          >
+            <Field
+              name='name'
+              type='text'
+              label='Event Name'
+              placeholder='Event Name'
+              component={this.renderField}
+              validate={required}
+            />
+            <Field
+              name='description'
               rows={3}
-              name="description"
-              placeholder="Write Event Description Here" />
-          </Form.Group>
+              type='text'
+              label='Description'
+              placeholder='Description'
+              component={this.renderField}
+            />
 
-          <Form.Group controlId="poster">
+            <Field
+              name='eventTime'
+              type='datetime-local'
+              label='Select Event Date'
+              placeholder='Select Event Date'
+              component={this.renderField}
+              validate={[required]}
+            />
+
             <Form.File
-              onChange = {handleChange}
-              name="poster"
+              name='poster'
+              label='Poster'
+              type='hidden'
               id="custom-file"
-              label="Upload Poster Image"
-              custom/>
-          </Form.Group>
+              placeholder='Add Poster Image'
+              component={this.renderField}
+              value={this.props.eID}
+            />
 
-          </Form>
-          </Col>
-          <Col>
-          <Form>
+            <Field
+              name='location'
+              type='text'
+              label='Location'
+              placeholder='Location'
+              component={this.renderField}
+            />
 
-          <Form.Group controlId="eventTags">
-            <Form.Label>Choose Event Tags</Form.Label>
+            <Field
+              name='email'
+              type='email'
+              id="custom-file"
+              label='Email'
+              placeholder='Enter Email'
+              validate={[required]}
+              component={this.renderField}
+              value={this.props.eID}
+            />    
+
+          <Form.Group>
             <Multiselect
+            name='tags'
+            placeholder='Tags'
             options={tags} // Options to display in the dropdown
-            onSelect={onTagSelect}
-            onRemove={onTagSelect} // Function will trigger on remove event
             displayValue="name" // Property name to display in the dropdown options
           />
           </Form.Group>
 
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              onChange = {handleChange}
-              name="email"
-              type="email"
-              placeholder="Enter email" />
-          </Form.Group>
-          </Form>
 
-          <Button variant="danger" type="submit" onClick = {handleSubmit}>
-            Submit
-          </Button>
-          </Col>
-</Row>
+            <Button variant="danger" type="submit">
+              Submit
+            </Button>
+          </form>
 
-</Container>
-</Jumbotron>
-</Container>
+        </Container>
       </div>
     );
+  }
 }
+console.log(addEventAction);
 
-export default UploadEvent;
+const required = value => (value ? undefined : 'Required');
+
+UploadEvent = connect(
+  null,
+  { addEventAction }
+)(UploadEvent);
+
+export default reduxForm({
+  form: 'eventForm'
+})(UploadEvent);
