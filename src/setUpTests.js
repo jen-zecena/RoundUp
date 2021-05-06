34eeb85295cfcc269518cf369c703010a0b5559b
peibarthwu <54913759+peibarthwu@users.noreply.github.com>
2021-05-06 09:40:40 -0700

    tests

--- src/setUpTests.js
+++ src/setUpTests.js
@@ -1,7 +1,1 @@
 import { configure } from 'enzyme';
-<<<<<<< HEAD
-=======
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
->>>>>>> f27eafac89314b7686579ed4c28ae624918da2dd
